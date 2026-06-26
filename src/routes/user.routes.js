02458dd8098940f49123229a3e54c6250b70a1e6
Ayush Verma <vermaayush9170@gmail.com>
2026-06-26 23:04:03 +0530

    refactor user routes to secure endpoints and update avatar/cover image handling

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,10 +2,14 @@ import { Router } from "express";
 import {
   changeCurrentPassword,
   getCurrUser,
+  getUserChannelProfile,
+  getUserWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateCoverImage,
+  updateUserAvatar,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -30,19 +34,16 @@ router.route("/login").post(loginUser);
 // secured route
 router.route("/logout").get(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/password-reset").post(changeCurrentPassword);
-router.route("/getUser").get(getCurrUser);
-router.route("/change-avatar").post(
-  upload.field({
-    name: "avatar",
-    maxCount: 1,
-  })
-);
-router.route("/change-coverImage").post(
-  upload.field({
-    name: "coverImage",
-    maxCount: 1,
-  })
-);
+router.route("/password-reset").post(verifyJWT, changeCurrentPassword);
+router.route("/getUser").get(verifyJWT, getCurrUser);
+router
+  .route("/update-avatar")
+  .post(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-coverImage")
+  .post(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getUserWatchHistory);
 
 export default router;
